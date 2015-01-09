18c0703b3bc87e45561ba4966fa4af5375172737
Wachter Johannes <johannes.wachter@massiveart.at>
2015-01-09 20:00:21 +0100

    added navigation generation

--- Admin/AsapoSuluFormAdmin.php
+++ Admin/AsapoSuluFormAdmin.php
@@ -5,11 +5,20 @@ namespace Asapo\Bundle\Sulu\FormBundle\Admin;
 use Sulu\Bundle\AdminBundle\Admin\Admin;
 use Sulu\Bundle\AdminBundle\Navigation\Navigation;
 use Sulu\Bundle\AdminBundle\Navigation\NavigationItem;
+use Sulu\Component\Security\UserInterface;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class AsapoSuluFormAdmin extends Admin
 {
-    public function __construct($title)
+    /**
+     * @var SecurityContextInterface
+     */
+    private $securityContext;
+
+    public function __construct($title, $forms, SecurityContextInterface $securityContext)
     {
+        $this->securityContext = $securityContext;
+
         $rootNavigationItem = new NavigationItem($title);
 
         $section = new NavigationItem('');
@@ -17,13 +26,31 @@ class AsapoSuluFormAdmin extends Admin
         $dropDown->setIcon('mail-forward');
         $section->addChild($dropDown);
 
-        $contactItem = new NavigationItem('Kontakt');
-        $contactItem->setAction('form/contact');
-        $dropDown->addChild($contactItem);
+        $user = $this->getUser();
+        $userLocale = $user->getLocale();
+
+        foreach ($forms as $formName => $form) {
+            if (array_key_exists('backend', $form)) {
+                $item = $this->getItem($formName, $form, $userLocale);
+                $dropDown->addChild($item);
+            }
+        }
 
         $rootNavigationItem->addChild($section);
-
         $this->setNavigation(new Navigation($rootNavigationItem));
+    }
+
+    private function getItem($formName, $form, $locale)
+    {
+        $title = ucfirst($formName);
+        if (array_key_exists('title', $form['backend']) && array_key_exists($locale, $form['backend']['title'])) {
+            $title = $form['backend']['title'][$locale];
+        }
+
+        $item = new NavigationItem($title);
+        $item->setAction('form/' . $formName);
+
+        return $item;
     }
 
     /**
@@ -32,6 +59,23 @@ class AsapoSuluFormAdmin extends Admin
     public function getJsBundleName()
     {
         return 'asaposuluform';
+    }
+
+    /**
+     * Get a user from the Security Context
+     * @return UserInterface
+     */
+    private function getUser()
+    {
+        if (null === ($token = $this->securityContext->getToken())) {
+            return null;
+        }
+
+        if (!is_object($user = $token->getUser())) {
+            return null;
+        }
+
+        return $user;
     }
 
 }
